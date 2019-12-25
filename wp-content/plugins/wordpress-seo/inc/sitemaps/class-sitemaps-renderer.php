229b992c0a30dde5fc4d18cd8130045c6807694e
Ruslan Katsalap <ruslan.kacalap@gmail.com>
2019-12-25 17:59:35 +0200

    xml header fix

--- wp-content/plugins/wordpress-seo/inc/sitemaps/class-sitemaps-renderer.php
+++ wp-content/plugins/wordpress-seo/inc/sitemaps/class-sitemaps-renderer.php
@@ -102,7 +102,7 @@ class WPSEO_Sitemaps_Renderer {
 		$urlset = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" '
 			. 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd '
 			. 'http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" '
-			. 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+			. 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="https://www.w3.org/1999/xhtml">' . "\n";
 
 		/**
 		 * Filters the `urlset` for a sitemap by type.
