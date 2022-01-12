b8e686e7935a81839d60ebb31f0c1cffbe147490
keepcalmdev <70899875+keepcalmdev@users.noreply.github.com>
2022-01-12 21:30:27 +0200

    Update config.php
    Remove AWS Key

--- wp-content/plugins/wp-aws-s3/lib/aws/.aws/credentials/config.php
+++ wp-content/plugins/wp-aws-s3/lib/aws/.aws/credentials/config.php
@@ -1,3 +1,3 @@
 [default]
-aws_access_key_id = AKIAXBCMJ5GQJLD4FA3A
-aws_secret_access_key = 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
+aws_access_key_id = ********************
+aws_secret_access_key = *************************************
